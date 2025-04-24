3719c73b2dfca7b57df7712cc456e221f908ddcd
bhargavdangar2004@gmail.com <bhargavdangar2004@gmail.com>
2025-04-24 11:42:00 +0530

    changes

--- budget traker/app.js
+++ budget traker/app.js
@@ -2,17 +2,20 @@ const express = require('express')
 const cors = require('cors')
 const connectDB = require('./db')
 
-const app = express()
-app.use(cors())
-app.use(express.json())
 
-connectDB()
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+connectDB();
 
 const expenseRoutes = require('./routes/expense')
 const budgetRoutes = require('./routes/budget')
+const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/expenses', expenseRoutes)
 app.use('/api/budgets', budgetRoutes)
+app.use('/register',userRoutes)
 
 
 
